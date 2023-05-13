9ac59cb249e9d15945c465215fa4dfde3f893eca
paulneo <jpaulneo2k@gmail.com>
2023-05-12 22:09:52 -0500

    text

--- src/Views/Weather/Calendar/tab.tsx
+++ src/Views/Weather/Calendar/tab.tsx
@@ -6,8 +6,8 @@ interface TabType {
 const tab: React.FC<TabType> = ({ title }) => {
   return (
     <div className="cursor-pointer group">
-      <p className="px-5 text-lg font-semibold pt-10 text-white-dark	">{title}</p>
-      <span className="block w-0 h-1	bg-yellow-500 mt-1 transition-all	group-hover:w-full"></span>
+      <p className="px-5 text-lg hover:font-bold pt-10 text-primary text-white-dark	">{title}</p>
+     <span className="block w-0 h-1	bg-yellow-500 mt-1 transition-all	group-hover:w-full"></span>
     </div>
   );
 };
